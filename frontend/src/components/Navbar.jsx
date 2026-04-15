1f5669f835c3019c1177267156e19bb7042f6929
varun01384 <varunjaine1@gmail.com>
2026-04-15 20:38:06 +0530

    Update Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+      <div style={{ padding: "10px", background: "#eee" }}>
+        <h2>Recruitix</h2>
+      </div>
+    );
+  };
+  
+  export default Navbar;
